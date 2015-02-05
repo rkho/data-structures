8fc22f7e582833efe7d1584fbf47f02d11e3eedb
rkho <rkho@me.com>
2015-02-04 20:30:58 -0800

    Started on LinkedList.js looks like we have functionality for adding new tails and refer to previous tails in a linear fashion.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,6 +4,20 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value);
+    list[value] = node;
+    if (!list.head){
+      list.head = node;
+    }
+
+    // Take the value of tail, and make next of the value to be the new Node.
+
+    if (!_.isNull(list.tail)){
+        list.tail.next = node;
+      };
+    list.tail = node;
+    // Somewhere here, we need to tell the LAST added to tail that it needs to be pointing to this NEW one.
+    return list.tail;
   };
 
   list.removeHead = function(){
@@ -24,6 +38,23 @@ var Node = function(value){
   return node;
 };
 
+//
+
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// {"a":"b", "b":"c", "c":"d", "d":"g", "g":"a"}
+// {0:"a", 1:"b", 2:"c", 3:"d"}
+
+
+// {0:"a"} // key: 0 value = "a"
+// {1:"b"} // key 1 value = "b"
+
+// list[0][value]
+// list[0][next] = list[1]
+
+// node.value
+// node.next
+
+// node.next = node.value
